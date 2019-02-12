f61b1985acc1939ec378725cf47fbdc903020634
Huang Xingqiao <a92304a92304@gmail.com>
2019-02-12 21:51:28 +0800

    good。

--- src/js/app.js
+++ src/js/app.js
@@ -3,18 +3,29 @@ const mcip = new Vue({
   data: {
     isNavShrink: false,
     isLineAppScreenshotShrink: false,
+    logoKey: 0,
   },
   mounted () {
     this.setShrink()
+    this.logoKey++
     console.log(`Hey! You found me ☺️! `)
   },
   methods: {
     setShrink () {
+
       window.addEventListener('scroll', (e) => {
         const top = document.scrollingElement.scrollTop || document.documentElement.scrollTop
-        this.isNavShrink = top > 200
-        this.isLineAppScreenshotShrink = top < 100
+        this.isNavShrink = this.$refs.lineAppSection.getBoundingClientRect().top < 100
+        if (this.$refs.lineAppSection)
+          this.isLineAppScreenshotShrink = this.$refs.lineAppSection.getBoundingClientRect().top > 200
       })
     },
+    getFacebookLink (id) {
+      const device = new MobileDetect(window.navigator.userAgent)
+
+      if (device.is(`iOS`)) return `fb://page/?id=${id}`
+      else if (device.is(`AndroidOS`)) return `fb://page/${id}`
+      else return `https://www.facebook.com/${id}`
+    }
   }
 })
