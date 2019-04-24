dcd7b52d94b55f6c3d0aeb31454dd42eb29c1264
Huang Xingqiao <a92304a92304@gmail.com>
2019-04-24 15:03:29 +0800

    新增聯絡我們欄位。

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ const mcip = new Vue({
     isNavShrink: false,
     isLineAppScreenshotShrink: false,
     isEnvelopeShrink: false,
+    contact: { name: '黃省喬', email: 'test@nobody.com',   phone: '', content:'' },
+    contactStatus: 0, // 0: 預設, 1: 傳送中, 2: 成功
+    errorMessage: null,
   },
   mounted () {
     this.setShrink()
@@ -17,7 +20,6 @@ const mcip = new Vue({
         if (this.$refs.lineAppSection && this.$refs.contactSection) {
           this.isLineAppScreenshotShrink = this.$refs.lineAppSection.getBoundingClientRect().top > 200
           this.isEnvelopeShrink = this.$refs.contactSection.getBoundingClientRect().top > 500
-          console.log(this.$refs.contactSection.getBoundingClientRect().top)
         }
       })
     },
@@ -27,6 +29,25 @@ const mcip = new Vue({
       if (device.is(`iOS`)) return `fb://page/?id=${id}`
       else if (device.is(`AndroidOS`)) return `fb://page/${id}`
       else return `https://www.facebook.com/${id}`
+    },
+    submitContact () {
+      this.errorMessage = null
+      this.contactStatus = 1
+      fetch('https://us-central1-mc-integration-platform.cloudfunctions.net/firestoreContact', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(this.contact)
+      })
+        .then(res => {
+          if (!res.ok)
+            throw Error(res.statusText)
+          return res
+        }).then((jsonData) => {
+          this.contactStatus = 2
+        }).catch((e) => {
+          this.errorMessage = `發生了一些問題，請稍後再試`
+          this.contactStatus = 0
+        })
     }
   }
 })
